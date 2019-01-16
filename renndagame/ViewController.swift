c10865d2125abff47d55f04e3f2eff848e1407ff
古長谷鷹念 <takane@kohasetakanennoMacBook-Pro.local>
2019-01-16 19:12:48 +0900

    Initial Commit

---
+++ renndagame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  renndagame
+//
+//  Created by 古長谷鷹念 on 2019/01/16.
+//  Copyright © 2019 古長谷鷹念. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
